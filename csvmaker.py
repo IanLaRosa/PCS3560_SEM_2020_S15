2fe99bd5af6d2e535bebf6730e24b54e3f7f56f5
IanLaRosa <ian.rosa@usp.br>
2020-08-31 20:06:45 -0300

    Versão 1.0
    - scaper.py 1.0
    - csvmaker.py 1.0
    - gitignore ajustado

---
+++ csvmaker.py
@@ -0,0 +1,51 @@
+import csv, os, re
+from os import listdir
+
+def text_treatment(test_str):
+    ret = ''
+    skip1c = 0
+    skip2c = 0
+    for i in test_str:
+        if i == '[':
+            skip1c += 1
+        elif i == '(':
+            skip2c += 1
+        elif i == ']' and skip1c > 0:
+            skip1c -= 1
+        elif i == ')'and skip2c > 0:
+            skip2c -= 1
+        elif skip1c == 0 and skip2c == 0:
+            ret += i
+    ret = ret.replace('\"','').replace('“','').replace('ʽ','').replace('ʼ','').replace('”','').replace('?','').replace('!','').replace('\'','').replace('*','').replace('\n', ' ')
+    temp_list = list(ret.split())
+    temp_list = [x for x in temp_list if "http" not in x and "www" not in x and ".com" not in x and ".br" not in x]
+    ret = ' '.join(temp_list)
+    ret = re.sub(r"(?<!\d)\.(?!\d)", ' . ', ret)
+    ret = ret.replace(',',' , ').replace(':','')
+    if not ret.endswith('.') or not ret.endswith(' . '):
+        ret = ret + ' . '
+    return ret
+
+text_files_dir = os.getcwd() + '\\artigos'
+raw_text_files = [f for f in listdir(text_files_dir)]
+
+sentence_counter = 1
+word_list = []
+sentence_count_list = []
+
+for text_file in raw_text_files :
+    with open(text_files_dir +'\\'+ text_file,'r', encoding='utf-8', errors='ignore') as file:
+        word_list.extend(list(text_treatment(file.read()).split()))
+
+word_list = list(filter(None, word_list))
+
+for word in word_list :
+    sentence_count_list.append(sentence_counter)
+    if word == '.' :
+        sentence_counter += 1
+
+text_file = open(os.getcwd() + '\\ner_dataset.txt',"w", encoding="utf-8")
+for i in range(len(word_list)) :
+    text_file.write(str(sentence_count_list[i])+';'+str(word_list[i])+';0'+'\n')
+text_file.flush()
+text_file.close()
--- scraper.py
+++ scraper.py
@@ -1,14 +1,96 @@
-import urllib.request
+import requests
+import os
+import numpy
+from bs4 import BeautifulSoup
 
-pageURL = "https://g1.globo.com/"
+def get_estadao():
 
-fp = urllib.request.urlopen(pageURL)
-mybytes = fp.read()
+    url = "https://www.estadao.com.br/"
+    url_request = requests.get(url)
+    landing_page = url_request.content
 
-baseHTMLstring = mybytes.decode("utf8")
-fp.close()
+    soup = BeautifulSoup(landing_page, 'html.parser')
 
-if(baseHTMLstring.find('currículo') != -1):
-    print("SIM")
-else:
-    print("NÃO")
+    landing_page_news = soup.find_all('div', class_='intro') 
+    soft_news = soup.find_all('section', class_='lista-soft')
+    for n in numpy.arange(0, len(landing_page_news)):
+
+        news_element = landing_page_news[n].find('a')
+        news_url = news_element['href']
+        if news_url is None:
+            continue
+        print(str(n) + " " + news_url)
+        if "einvestidor" in news_url or "infograficos" in news_url or "paladar" in news_url or "estadao" not in news_url:
+            print("NON-REGULAR ARTICLE, SKIPPED")
+            continue
+
+        news_request = requests.get(news_url)
+        news_page = news_request.content
+        news_soup = BeautifulSoup(news_page, 'html.parser')
+        news_body = news_soup.find('div', class_='n--noticia__content content')
+        if news_body is None:
+            continue
+        full_news = news_body.text
+
+        try:
+            children = news_body.findChildren('div', class_='n--noticia__citacao')
+            for child in children:
+                full_news = full_news.replace(child.text, '')
+        except Exception as e:
+            print("\n ERROR FINDING CHILDREN: " + str(e))
+
+        text_file = open("artigos/"+str(news_url).replace('/','_').replace(':','_')+".txt","w", encoding="utf-8")
+        text_file.write(str(full_news))
+        text_file.flush()
+        text_file.close()
+
+def get_folha():
+    url = "https://www.folha.uol.com.br/"
+    url_request = requests.get(url)
+    landing_page = url_request.content
+
+    soup = BeautifulSoup(landing_page, 'html.parser')
+
+    main_news = soup.find_all('href', class_='c-main-headline__url') 
+    panel_news = soup.find_all('div', class_='c-list-links__content')
+    secondary_news = soup.find_all('div', class_='c-headline__content')
+    landing_page_news = main_news + panel_news + secondary_news
+
+    for n in numpy.arange(0, len(landing_page_news)):
+
+        news_element = landing_page_news[n].find('a')
+        news_url = news_element['href']
+        if news_url is None:
+            continue
+        print(str(n) + " " + news_url)
+
+        news_request = requests.get(news_url)
+        news_page = news_request.content
+        news_soup = BeautifulSoup(news_page, 'html.parser', from_encoding="utf-8")
+        news_body = news_soup.find('div', class_='c-news__body')
+        if news_body is None:
+            continue
+        full_news = news_body.text
+
+        try:
+            children = news_body.findChildren('div', class_='js-gallery-widget rs_skip')
+            for child in children:
+                full_news = full_news.replace(child.text, '')
+        except Exception as e:
+            print("\n ERROR FINDING CHILDREN: " + str(e))
+
+        text_file = open("artigos/"+str(news_url).replace('/','_').replace(':','_')+".txt","w", encoding="utf-8")
+        text_file.write(str(full_news))
+        text_file.flush()
+        text_file.close()
+
+current_dir = os.getcwd()
+if not os.path.exists(current_dir + '/artigos'):
+    os.makedirs(current_dir + '/artigos')
+print("_____________________________________________________________________\n")
+print("                  AGREGADOR DE NOTÍCIAS\n")
+print("_____________________________________________________________________\n")
+print("ESTADÃO :")
+get_estadao()
+print("FOLHA :")
+get_folha()
